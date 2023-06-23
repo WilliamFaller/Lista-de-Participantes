6d2a0d440a37e95beddd9fbc0bb1d3d8225b61b5
WilliamFaller <mew.pvp10@gmail.com>
2023-06-22 22:36:15 -0300

    Componentizando elementos

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      padding: 24,
+    },
+    eventName: {
+      color: '#fff',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48,
+    },
+    eventDate: {
+      color: '#6B6B6B',
+      fontSize: 16,
+    },
+  });
