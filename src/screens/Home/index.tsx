40ac233c0825930588ab90677c6e7ad9fe0c9108
WilliamFaller <mew.pvp10@gmail.com>
2023-06-25 18:57:05 -0300

    useState

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,57 +1,57 @@
+import { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
 
 export default function Home() {
 
-  const participants = ['William', 'Rodrigo', 'Diego', 'Vini', 'Biro', 'Ana', 'Isa', 
-  'Jack', 'Mayk', 'Guilherme', 'João', 'Pedro', 'Lucas', 'Mateus']
+  const [participants, setParticipants] = useState(['João']);
 
-    function handleParticipantAdd() {
-      if(participants.includes('William')){
-        return Alert.alert('Participante existente', 'Já existe um participante na lista com esse nome.')
-      }
-        console.log('Adicionou um participante!');
+  function handleParticipantAdd() {
+    if (participants.includes('William')) {
+      return Alert.alert('Participante existente', 'Já existe um participante na lista com esse nome.')
     }
+    setParticipants(prevState => [...prevState, 'William']);
+  }
 
-    function handleParticipantRemove(name: string) {
-      Alert.alert('Remover participante', `Deseja remover ${name} da lista de participantes?`, [{
-        text: 'Sim',
-        onPress: () => Alert.alert('Removido com sucesso!')
-      },
-      {
-        text: 'Não',
-        style: 'cancel',
-        //onPress: () => Alert.alert('Remoção cancelada!')
-      }
+  function handleParticipantRemove(name: string) {
+    Alert.alert('Remover participante', `Deseja remover ${name} da lista de participantes?`, [{
+      text: 'Sim',
+      onPress: () => Alert.alert('Removido com sucesso!')
+    },
+    {
+      text: 'Não',
+      style: 'cancel',
+      //onPress: () => Alert.alert('Remoção cancelada!')
+    }
     ])
-      console.log(`Você removeu ${name} da lista de participantes`);
-    }
+    console.log(`Você removeu ${name} da lista de participantes`);
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento!</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
       <View style={styles.form}>
-        <TextInput 
-            placeholder='Nome do participante'
-            placeholderTextColor={'#6b6b6b'}
-            style={styles.input}
-            keyboardType='default'
+        <TextInput
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6b6b6b'}
+          style={styles.input}
+          keyboardType='default'
         />
-        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}> 
-            <Text style={styles.buttonText}>+</Text>
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
-      <FlatList 
+      <FlatList
         keyExtractor={item => item}
         data={participants}
         renderItem={({ item }) => (
-          <Participant 
-          key={item} 
-          name={item} 
-          onRemove={() => handleParticipantRemove(item)}
-        />
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
         )}
         showsVerticalScrollIndicator={false}
         ListEmptyComponent={() => (
@@ -59,7 +59,7 @@ export default function Home() {
             Não há participantes cadastrados, seja o primeiro a adicionar um participante
           </Text>
         )}
-        />
+      />
 
     </View>
   );
